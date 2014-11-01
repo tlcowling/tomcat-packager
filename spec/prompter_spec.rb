3bf1b16d553dbca681ac87577fbbd29a401c3680
Tom Cowling <tom.cowling@gmail.com>
2014-11-01 14:27:36 +0000

    Ah TDD I like you but I really sometimes just want to go fast... I know youre quicker in the long run, and when working with people...

--- spec/prompter_spec.rb
+++ spec/prompter_spec.rb
@@ -3,8 +3,18 @@ require 'spec_helper'
 require_relative '../lib/prompter.rb'
 
 describe 'Prompter' do
-  it 'should do something' do
-    test_prompter = Prompter.new
+  before :each do
+    @test_prompter = Prompter.new
+  end
 
+  it 'should introduce itself to the user' do
+    pending('message to user')
+    fail
+  end
+
+  it 'should save user choice as json' do
+    pending('fill it with some json then save it')
+    @test_prompter.save_as_json
+    fail
   end
 end
