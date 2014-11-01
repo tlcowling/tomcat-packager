5811c6671364212efeaa43c26eb2ac04608ca4fc
Tom Cowling <tom.cowling@gmail.com>
2014-11-01 14:38:59 +0000

    Write that json

--- lib/prompter.rb
+++ lib/prompter.rb
@@ -3,12 +3,21 @@
 # Prompt the user for some criteria
 
 require 'json'
+require 'highline/import'
 
 class Prompter
-  def self.begin
-
+  def initialize
+    start
   end
-  def save_as_json
 
+  def start
+    parameters = {}
+    parameters[:version] = ask('What version of Tomcat?') { |q| q.default = "7.0.56" } 
+    save_as_json(parameters)
+  end
+
+  def save_as_json parameters
+    json = parameters.to_json
+    File.write 'test.json', json
   end
 end
