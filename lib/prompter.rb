048bfb831df640d089af133c5e66f739b7aa924a
Tom Cowling <tom.cowling@gmail.com>
2014-11-01 14:48:40 +0000

    Extract version method to make it more readable

--- lib/prompter.rb
+++ lib/prompter.rb
@@ -10,13 +10,18 @@ class Prompter
     start
   end
 
+  def major_version version_string
+    version_string.match(/\d+/)[0]
+  end
+
   def start
     parameters = {}
     parameters[:version] = ask('What version of Tomcat?', String) { |q| q.default = "7.0.56" } 
-    major_version = parameters[:version].match(/\d+/)[0]
 
-    parameters[:user] = ask('Tomcat user?', String) { |q| q.default = "tomcat#{major_version}" }
-    parameters[:group] = ask('Tomcat group?', String) { |q| q.default = "tomcat#{major_version}" }
+    version = major_version parameters[:version]
+
+    parameters[:user] = ask('Tomcat user?', String) { |q| q.default = "tomcat#{version}" }
+    parameters[:group] = ask('Tomcat group?', String) { |q| q.default = "tomcat#{version}" }
     save_as_json(parameters)
   end
 
