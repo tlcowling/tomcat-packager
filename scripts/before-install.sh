72a40bd34a95fad47091de7aa1cb52144ad3c84b
Tom Cowling <tom.cowling@gmail.com>
2014-11-04 15:37:07 +0000

    get java home in the upstart script

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
-# Creates the tomcat users
+# Creates the tomcat users if they don't already exist
+# Creates a log directory for the service
 #
 set -e
 
 id -u tomcat &>/dev/null || sudo useradd tomcat
 id --group tomcat &>/dev/null || sudo groupadd tomcat
+
+mkdir -p /var/log/tomcat
+chown tomcat:tomcat /var/log/tomcat
