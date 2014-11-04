51f591290ee6827ca4296cb40db9961e82370877
Tom Cowling <tom.cowling@gmail.com>
2014-11-04 14:24:40 +0000

    Bugfix where service wouldnt shut down after uninstall

--- scripts/before-remove.sh
+++ scripts/before-remove.sh
@@ -2,11 +2,13 @@
 #
 # Stops the tomcat service
 #
-set -e
-
 if [ -f /etc/init/tomcat.conf ]; then
+  echo "checking if tomcat is running..."
   service tomcat status | grep "running"
   if [ $? != 1 ]; then
+    echo "stopping tomcat since it was running"
     service tomcat stop
   fi
 fi
+
+exit 0
